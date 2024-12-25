858c928d7b59eea020ed1fd9ea231b19518cd0fd
koushik <loadagalakoushik1510@gmail.com>
2024-12-25 16:36:34 +0530

    changed path tolivein api url

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:4200"
+// export const API_URL = "http://localhost:4200"
 
-// export const API_URL = "https://backend-foodie.onrender.com"
+ export const API_URL = "https://backend-foodie.onrender.com"
 
